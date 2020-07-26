4f9605ccd75af15a85143b0211282d1729da2bfc
Muescha <184316+muescha@users.noreply.github.com>
2020-07-26 04:49:08 +0200

    refactor: add debug package

--- index.js
+++ index.js
@@ -1,59 +1,64 @@
-'use strict';
+"use strict"
 
-const chalk = require('chalk');
-const rule = require('unified-lint-rule');
-const minimatch = require('minimatch');
+const debug = require("debug")("vfile-message-control")
+const chalk = require("chalk")
+const rule = require("unified-lint-rule")
+const minimatch = require("minimatch")
 
-module.exports = rule('remark-lint:ignore-messages', ignoreMessages);
+module.exports = rule("vfile-message-control", ignoreMessages)
+
+const info_deny = chalk.red("deny ")
+const info_allow = chalk.green("allow")
+const info_filename = '            %s --> %s'
+const info_rule = `Rule: %s ${chalk.yellow('%s')}`
 
 function ignoreMessages(ast, file, options) {
 
   if (!options || !options.deny) {
-    return;
+    return
   }
 
-  const optionName = options.optionName || 'id';
-  const stripPathPrefix = options.stripPathPrefix || [];
+  const optionName = options.optionName || "id"
+  const stripPathPrefix = options.stripPathPrefix || []
 
   file.messages = file.messages.filter((message) => {
-    const id = name(message, optionName, stripPathPrefix);
-    const remove = checkRemove(id);
-    return !remove;
-  });
+    const id = name(message, optionName, stripPathPrefix)
+    const remove = checkRemove(id)
+    return !remove
+  })
+
 
   function checkRemove(id) {
+
     for (const option of options.deny) {
-      if (minimatch(id, option, { nocase: true, debug: false })) {
-        if (options.debug) {
-          console.log(id + chalk.red(' --> deny '));
-          console.log(`Rule: deny ${chalk.yellow(option)}`);
-        }
+      if (minimatch(id, option, {nocase: true, debug: false})) {
+
+        debug(info_rule, info_deny, option)
 
         if (options.allow) {
           for (const option of options.allow) {
-            if (minimatch(id, option, { nocase: true, debug: false })) {
-              if (options.debug) {
-                console.log(id + chalk.green(' --> allow '));
-                console.log(`Rule: allow ${chalk.yellow(option)}`);
-              }
-              return false;
+            if (minimatch(id, option, {nocase: true, debug: false})) {
+              debug(info_rule, info_allow, option)
+              debug(info_filename, id, info_allow)
+              return false
             }
           }
         }
-        return true;
+        debug(info_filename, id, info_deny)
+        return true
       }
     }
-    return false;
+    return false
   }
 
   function name(message, optionName, stripPathPrefix) {
-    let name = message.name;
+    let name = message.name
     stripPathPrefix.forEach((v) => {
       if (name.startsWith(v)) {
-        name = name.substring(v.length);
+        name = name.substring(v.length)
       }
-    });
-    const option = message[optionName] ? (':' + message[optionName]) : '';
-    return `${name}:${message.source}:${message.ruleId}${option}`;
+    })
+    const option = message[optionName] ? (":" + message[optionName]) : ""
+    return `${name}:${message.source}:${message.ruleId}${option}`
   }
 }
