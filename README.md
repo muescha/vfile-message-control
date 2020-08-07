dc0800f128d0dc8cb2622e91efd02d69bdfa3355
Muescha <184316+muescha@users.noreply.github.com>
2020-08-07 17:17:19 +0200

    add check for message text

--- README.md
+++ README.md
@@ -12,11 +12,11 @@ the id is calculated from the filename and rule name and the location of the mes
 
 ### Option `deny`
 
-an array of minimatch globs to remove messages  
+an array of `matcher` to remove messages  
 
 ### Option `allow`
 
-an array of minimatch globs to keep messages (evaluated after deny)  
+an array of `matcher` to keep messages (evaluated after deny)  
 
 ### Option `stripPathPrefix`
 
@@ -24,7 +24,28 @@ an array remove from the start of the calculated id
 
 ### Option `optionName`
 
-an string for an option appended to the id    
+an string for an option appended to the id
+
+### Parameter `matcher`
+
+always check the `globstring` against `id` and if this matched the second parameter check with parameter `message.message`, function check with parameter `message`
+
+- matcher := `string`
+- matcher := `[string [, (string|regexp|function)]]`
+
+can be this combinations:
+
+- Type 1: `matcher` :=  `globString` 
+- Type 2: `matcher` := [`globString`]
+- Type 3: `matcher` := [`globString`, `regexpString`]
+- Type 4: `matcher` := [`globString`, `regexp`]
+- Type 5: `matcher` := [`globString`, `function`]
+
+with this options:
+- `globString` := a string minimatch glob string
+- `regexpString` := a string which would converted to a RegExp
+- `rexexp` = a RegExp called with parameter  `.test(message.message)
+- `function` = a function called with parameter `message`
 
 ## Examples
 
@@ -36,6 +57,7 @@ Example configuration:
       {
         "deny":[
           "docs/docs/headless-cms-1.md:*:retext-repeated-words:*",
+          ["docs/docs/README.md:remar-lint:retext-repeated-words:*", '^The parameter'],
         ],
         "allow": [
           "docs/docs/headless-cms-1.md:*:retext-repeated-words:graphql",
@@ -45,6 +67,14 @@ Example configuration:
 ```
 
 ![Screenshot](./screenshot.png)
+
+## Loggin
+
+It uses the [`debug`](https://www.npmjs.com/package/debug) package, so you can enable loggin with
+
+```shell
+DEBUG=vfile-message-control remark
+```
 
 ## Note
 
--- index.js
+++ index.js
@@ -12,6 +12,62 @@ const info_allow = chalk.green("allow")
 const info_filename = '            %s --> %s'
 const info_rule = `Rule: %s ${chalk.yellow('%s')}`
 
+// Utils
+const isString = test => typeof test === "string";
+const isRegExp = test => test instanceof RegExp;
+const isFunction = test => test instanceof Function;
+const wrapArray = value => Array.isArray(value) ? value : [value]
+
+function getRegExp(regex) {
+  // special for plain strings: they are not "instanceof String"
+  if (isString(regex)) {
+    regex = new RegExp(regex);
+  }
+
+  if (isRegExp(regex)) {
+    throw Error('Required "regex" option need to be a RegEx or RegExp-String');
+  }
+  return regex
+}
+
+function getTest(test) {
+
+  if (!test) {
+    return () => false
+  }
+
+  if (isFunction(test)) {
+    return (message) => {
+      return test(message)
+    }
+  }
+
+  return (message) => {
+    return getRegExp(test).test(message.message)
+  }
+}
+
+
+function match(id, option, message) {
+
+  // Type 1: option := string
+  // Type 2: option := [string]
+  // Type 3: option := [string, string]
+  // Type 4: option := [string, regexp]
+  // Type 5: option := [string, function]
+
+  const [idMatch, testMatch] = wrapArray(option)
+
+  const matchedId = minimatch(id, idMatch, {nocase: true, debug: false})
+
+  if (matchedId && testMatch) {
+    return getTest(testMatch)(message)
+  } else {
+    return matchedId
+  }
+}
+
+
 function ignoreMessages(ast, file, options) {
 
   if (!options || !options.deny) {
@@ -22,22 +78,26 @@ function ignoreMessages(ast, file, options) {
   const stripPathPrefix = options.stripPathPrefix || []
 
   file.messages = file.messages.filter((message) => {
+
     const id = name(message, optionName, stripPathPrefix)
-    const remove = checkRemove(id)
-    return !remove
+    message.id = id
+
+    const removeMessage = checkRemove(id, message)
+    return !removeMessage
   })
 
 
-  function checkRemove(id) {
+  function checkRemove(id, message) {
+
 
     for (const option of options.deny) {
-      if (minimatch(id, option, {nocase: true, debug: false})) {
+      if (match(id, option, message)) {
 
         debug(info_rule, info_deny, option)
 
         if (options.allow) {
           for (const option of options.allow) {
-            if (minimatch(id, option, {nocase: true, debug: false})) {
+            if (match(id, option, message)) {
               debug(info_rule, info_allow, option)
               debug(info_filename, id, info_allow)
               return false
